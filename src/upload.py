41378aa11695d8e338fee63da095397f90f86d35
Lukas Scholtes <luke.scholtes@gmail.com>
2022-12-06 14:55:46 +0000

    Modifying user instructions

--- src/upload.py
+++ src/upload.py
@@ -8,11 +8,16 @@ from streamlit_cropper import st_cropper
 def upload_and_crop() -> Union[Image.Image, None]:
 
     with st.expander("File Upload"):
+        st.markdown("Please upload a top-down image of the receipt with good lighting")
+        st.markdown("**Note:** Mobile users should upload a pre-cropped image \
+            that shows only the actual receipt items - in-app cropping on \
+            mobile is coming soon."""
+        )
         receipt_image = st.file_uploader("Upload image")
         crop_image = st.checkbox(
             "Crop image?",
             help="Picture should contain receipt line entries only. \
-                MOBILE USERS, please pre-crop any uploaded pictures, \
+                **Mobile users**, please pre-crop any uploaded pictures, \
                 do not use this option.",
         )
         if receipt_image is None:
@@ -21,13 +26,13 @@ def upload_and_crop() -> Union[Image.Image, None]:
     receipt_image = Image.open(receipt_image)
 
     if crop_image:
-        left, right = st.columns([2, 1])
-        with left:
-            st.markdown("Original image")
-            cropped_image = st_cropper(receipt_image)
-        with right:
-            st.markdown("Line entries only")
-            st.image(cropped_image)
+        # left, right = st.columns([2, 1])
+        # with left:
+        st.markdown("Original image")
+        cropped_image = st_cropper(receipt_image)
+    # with right:
+        st.markdown("Line entries only")
+        st.image(cropped_image)
         return cropped_image
     else:
         st.markdown("Uploaded image")
